ba0c10e23f981439ec03fd273acb65075c63a493
Conrad <conradkaranja5@gmail.com>
2026-07-05 09:03:20 +0300

    create FoodItem model

--- calorieProject/settings.py
+++ calorieProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,11 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&n=so(l51p1$=274qi!n3gzpy42n-lc(u-+btvb&vn=3u!+a8='
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = config('DEBUG')
 ALLOWED_HOSTS = []
 
 
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'calorieProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("DATABASE_NAME"),
+        'USER' : config("DATABASE_USER"),
+        'PASSWORD' : config("DATABASE_PASS"),
+        'HOST' : config("DATABASE_HOST"),
+        'PORT' : config("DATABASE_PORT"),
+
     }
 }
 
