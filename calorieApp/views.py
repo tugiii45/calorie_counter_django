658e08168cbe8454e78123d073950df4c066a92d
Conrad <conradkaranja5@gmail.com>
2026-07-05 09:21:49 +0300

    implement add, delete, and reset logic

--- calorieApp/views.py
+++ calorieApp/views.py
@@ -1,19 +1,27 @@
-
 from django.shortcuts import render, redirect
 from .models import FoodItem
 from django.db.models import Sum
 
-# Create your views here.
-
 def index(request):
-    items = FoodItem.objects.all()
-    total_calories = items.aggregate(Sum('calories'))['calories__sum'] or 0
-    
     if request.method == 'POST':
-        # Logic to add a new item
-        name = request.POST.get('name')
-        calories = request.POST.get('calories')
-        FoodItem.objects.create(name=name, calories=calories)
-        return redirect('/')
+        # Add a new food item
+        if 'add' in request.POST:
+            name = request.POST.get('name')
+            calories = request.POST.get('calories')
+            FoodItem.objects.create(name=name, calories=calories)
         
-    return render(request, 'index.html', {'items': items, 'total': total_calories})
+        # Reset all items
+        elif 'reset' in request.POST:
+            FoodItem.objects.all().delete()
+            
+        # Remove a specific item
+        elif 'delete' in request.POST:
+            item_id = request.POST.get('item_id')
+            FoodItem.objects.filter(id=item_id).delete()
+            
+        return redirect('/')
+
+    # GET request: Display items and total
+    items = FoodItem.objects.all()
+    total = items.aggregate(Sum('calories'))['calories__sum'] or 0
+    return render(request, 'index.html', {'items': items, 'total': total})
