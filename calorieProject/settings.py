e63d01702bd0564e3fd7c9943edbf7eca96f24ad
Conrad <conradkaranja5@gmail.com>
2026-07-05 09:10:59 +0300

    create template for food list and add-item form

--- calorieProject/settings.py
+++ calorieProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'calorieProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['calorieApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
